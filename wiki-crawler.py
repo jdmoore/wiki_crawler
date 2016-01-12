68a86fec70539acf1582c887a68d002a59db928c
jdmoore <jdmoore1342@gmail.com>
2016-01-12 09:19:39 -0500

    Implemented scraping
    Added the scrape_citations(self) method to the Spider class.

--- wiki-crawler.py
+++ wiki-crawler.py
@@ -31,9 +31,11 @@ class Spider(object):
             self.current_soup = url_soup(start)
             self.current_page = str(self.current_soup.p.b)[3:-4]
             
-            
+        self.destination = self.current_url    
         self.max_distance = max_distance
         self.pages_visited = [(self.current_page, self.current_url)]
+        self.citations = dict()
+        self.scrape_citations()
 
         while len(self.pages_visited) <= self.max_distance:
             print(self.current_page + ":\n" + self.current_url)
@@ -47,12 +49,51 @@ class Spider(object):
         """Method to follow the first link of the first paragraph of a
         Wikipedia article.
         """
+        if len(self.pages_visited) <= self.max_distance:
+            soup = bs4.BeautifulSoup(str(self.current_soup.find('p')),
+                                     "html.parser")
+            #print("New Soup:\n" + str(soup) + "\n")
+            for link in soup.findAll('a'):
+                #print("\nLink:\n"+ str(link) +"\n")
+                if "Help:IPA" in link.get('href'):
+                    continue
+                else:
+                    self.destination = WIKIPEDIA + link.get('href')
+                    break
+            self.current_url = self.destination
+            self.current_soup = url_soup(self.destination)
+            self.current_page = str(self.current_soup.p.b)[3:-4]
+            self.pages_visited.append((self.current_page, self.current_url))
+            self.scrape_citations()
+        else:
+            print("Maximum depth reached.")
+
+    def scrape_citations(self):
+        """start_url is a string telling the crawler where to start."""
+        sources = []
+        soup = url_soup(self.current_url)
         
-        destination = WIKIPEDIA + self.current_soup.p.a.get('href')
-        self.current_url = destination
-        self.current_soup = url_soup(destination)
-        self.current_page = str(self.current_soup.p.b)[3:-4]
-        self.pages_visited.append((self.current_page, self.current_url))
+        citation_num = 1
+        for reference in soup.findAll('span', {'class': 'reference-text'}):
+            print("\n" + str(citation_num) + '.')
+            citation_soup = bs4.BeautifulSoup(str(reference), "html.parser")
+            #print(citation_soup)
+            citation = citation_soup.find('a')#, {'class': 'external text'})
+            if not citation:
+                source = str(citation_soup)
+            else:
+                #print("\n" + str(citation))
+                href = citation.get('href')
+                #print("href: " + str(href))
+                if citation.string is not None:
+                    title = citation.string
+                    source = title + ", " + href
+                else:
+                    source = "<Title Unavailable>, " + href
+            sources.append(source)
+            citation_num += 1
+        page_name = str(soup.p.b)[3:-4]
+        self.citations[page_name] = sources
         
         
 
@@ -68,5 +109,10 @@ def url_soup(url):
 
 
 page = 'https://en.wikipedia.org/wiki/Dishonored'
-spider = Spider()
-
+page2 = 'https://en.wikipedia.org/wiki/Marc_Bendavid'
+spider = Spider(start=page, max_distance=2)
+for key in spider.citations:
+    print(key + ":\n\n")
+    for citation in spider.citations[key]:
+        print(citation)
+        print("\n")
